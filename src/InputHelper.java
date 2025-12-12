e181d62a65a26308e04fc93d14f90dcdc3b8f700
SKern616 <kern.616902@swl.k12.oh.us>
2025-12-12 08:44:58 -0500

    Done

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,172 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    //getNonZeroLenString method returns a string of non-zero length back to user
+    public static String getNonZeroLenString(Scanner scan, String prompt) {
+        System.out.println(prompt);
+        boolean validInput = false;
+
+        String userInput;
+        do {
+            userInput = scan.nextLine();
+
+            if (userInput.length() > 0) {
+                validInput = true;
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter a non-zero length.");
+            }
+        } while (!validInput);
+
+        return userInput;
+    }
+
+    public static int getInt(Scanner scan, String prompt){
+        System.out.println(prompt);
+        boolean validInput = false;
+        int userInput = 0;
+
+        do {
+            if (scan.hasNextInt()){
+                userInput = scan.nextInt();
+                validInput = true;
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter a valid integer:");
+                scan.nextLine();
+            }
+        } while (!validInput);
+
+        return userInput;
+    }
+
+    public static int getRangeInt(Scanner scan, String prompt, int min, int max){
+        System.out.println(prompt);
+        boolean validInput = false;
+        int userInput = 0;
+
+        do {
+            if (scan.hasNextInt()){
+                userInput = scan.nextInt();
+                if (userInput >= min && userInput <= max) {
+                    validInput = true;
+                } else {
+                    System.out.println("ERROR - Please enter a number between " + min + " and " + max + ":");
+                    scan.nextLine();
+                }
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter a valid integer:");
+                scan.nextLine();
+            }
+        } while (!validInput);
+
+        return userInput;
+    }
+
+    public static double getRangedDouble(Scanner scan, String prompt, double min, double max){
+        boolean valid = false;
+        double userInput = 0;
+
+        System.out.println(prompt);
+        do {
+
+            if (scan.hasNextDouble()){
+                userInput = scan.nextDouble();
+
+                if (userInput >= min && userInput <= max){
+                    valid = true;
+                } else {
+                    System.out.println("ERROR - Please enter a number between " + min + " and " + max + "!");
+                    scan.nextLine();
+                }
+            } else {
+                System.out.println("ERROR - Please enter a valid input!");
+                scan.nextLine();
+            }
+        } while (!valid);
+
+        return userInput;
+    }
+
+    public static int getPositiveNonZeroInt(Scanner scan, String prompt){
+        int userInput = 0;
+        boolean valid = false;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()){
+                userInput = scan.nextInt();
+
+                if (userInput > 0){
+                    valid = true;
+                } else {
+                    System.out.println("ERROR - Please enter a positive non-zero number!");
+                    scan.nextLine();
+                }
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter a valid non-zero integer!");
+                scan.nextLine();
+            }
+        } while(!valid);
+
+        return userInput;
+    }
+
+    public static String getRegExString(Scanner scan, String prompt, String regExPattern){
+        String userString;
+        boolean valid = false;
+        System.out.println(prompt);
+
+        do {
+            userString = scan.nextLine();
+            if (userString.matches(regExPattern)) {
+                valid = true;
+            } else {
+                System.out.println("ERROR - Invalid input! Please follow the pattern.");
+            }
+        } while (!valid);
+        return userString;
+    }
+
+    public static double getDouble(Scanner scan, String prompt){
+        System.out.println(prompt);
+        boolean valid = false;
+        double userInput = 0.0;
+        do {
+            if (scan.hasNextDouble()){
+                userInput = scan.nextDouble();
+                valid = true;
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter a valid number!");
+                scan.nextLine();
+            }
+        } while (!valid);
+
+        return userInput;
+    }
+
+    public static boolean getYNConfirm(Scanner scan, String prompt) {
+        System.out.println(prompt);
+        boolean valid = false;
+        boolean userChoice = false;
+        String userInput;
+
+        do {
+            if (!scan.hasNextInt() && !scan.hasNextDouble()) {
+                userInput = scan.nextLine();
+
+                if (userInput.equalsIgnoreCase("Y")) {
+                    userChoice = true;
+                    valid = true;
+                } else if (userInput.equalsIgnoreCase("N")) {
+                    valid = true;
+                } else {
+                    System.out.println("ERROR - Invalid choice! Please enter Y or N!");
+                }
+            } else {
+                System.out.println("ERROR - Invalid input! Please enter Y or N!");
+                scan.nextLine();
+            }
+        } while (!valid);
+
+        return userChoice;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,117 @@
-package PACKAGE_NAME;
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    static ArrayList <String> lists = new ArrayList<String>();
+
+    private static Scanner scan = new Scanner(System.in);
+
+    static boolean fileSaved = false;
+
+    public static void main(String[] args) throws IOException {
+        boolean userQuit = false;
+        boolean fileOpened = false;
+        boolean fileEdited = false;
+        boolean userClear = false;
+        String userChoice;
+
+        System.out.println("Welcome to ListMaker9000!");
+        arrayPrint();
+
+        do {
+
+            System.out.println("Choose an option from the menu:");
+            userChoice = InputHelper.getRegExString(scan, "A - Add an entry to the list\nD - Delete an entry from the list\nP - Print the list\nQ - Quit the program\nO - Open a list\nS - Save the list\nC - Clear the list", "[AaDdPpQqOoSsCc]");
+
+            if (userChoice.equalsIgnoreCase("A")) {
+                arrayAdd(scan);
+                fileEdited = true;
+            }
+
+            if (userChoice.equalsIgnoreCase("D")) {
+                arrayDelete(scan);
+                fileEdited = true;
+            }
+
+            if (userChoice.equalsIgnoreCase("P")) {
+                arrayPrint();
+            }
+
+            if (userChoice.equalsIgnoreCase("Q")) {
+                userQuit = programQuit(scan);
+            }
+
+            if (userChoice.equalsIgnoreCase("O")){
+                clearList();
+                IOHelper.openFile(lists);
+                fileOpened = true;
+            }
+
+            if (userChoice.equalsIgnoreCase("S")){
+                System.out.println("Name your file or enter an existing file name:");
+                String fileName = scan.nextLine();
+
+                IOHelper.writeFile(lists, fileName);
+
+                System.out.println("Your file has been saved as: " + fileName);
+                fileSaved = true;
+            }
+
+            if (userChoice.equalsIgnoreCase("C")) {
+                userClear = InputHelper.getYNConfirm(scan, "Are you sure you'd like to clear the list?");
+                if (userClear) {
+                    clearList();
+                } else {
+                    System.out.println("Action canceled...");
+                }
+            }
+
+        } while (!userQuit);
+    }
+
+    private static void arrayAdd(Scanner scan){
+        String newString = InputHelper.getNonZeroLenString(scan, "What would you like to add?:");
+        lists.add(newString);
+        fileSaved = false;
+    }
+
+    private static void arrayDelete(Scanner scan){
+        int index = InputHelper.getRangeInt(scan, "What would you like to remove? (Starting at 0): ", 0, lists.size() - 1);
+        lists.remove(index);
+        scan.nextLine();
+        fileSaved = false;
+    }
+
+    private static void arrayPrint(){
+
+        if (lists.isEmpty()) {
+            System.out.println("List is empty!");
+        } else {
+            for (int i = 0; i < lists.size(); i++) {
+                System.out.println(i + " - " + lists.get(i));
+            }
+        }
+
+    }
+
+    private static boolean programQuit(Scanner scan) {
+        String prompt;
+        if (!fileSaved) {
+            prompt = "You have unsaved changes. Are you sure would you like to quit?";
+        } else {
+            prompt = "Are you sure you would like to quit?";
+        }
+        boolean userYN = InputHelper.getYNConfirm(scan, prompt);
+
+        return  userYN;
+    }
+
+    private static void clearList(){
+        lists.clear();
+    }
 }
